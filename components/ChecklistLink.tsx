5dcd5f055c229f62cb474683e22ac833659878ee
CrowsVeldt <zechy.mahler@gmail.com>
2024-11-27 06:47:34 -0700

    ui: improve ChecklistLink styles

--- components/ChecklistLink.tsx
+++ components/ChecklistLink.tsx
@@ -1,16 +1,35 @@
 import { ChecklistType } from "@/types";
-import { Link } from "expo-router";
-import { Text } from "react-native";
+import { router } from "expo-router";
+import { Pressable, StyleSheet, Text } from "react-native";
 
 export default function ChecklistLink({ list }: { list: ChecklistType }) {
   return (
-    <Link
-      href={{
-        pathname: "/checklist/[checklist]",
-        params: { checklist: list.id },
-      }}
+    <Pressable
+      onPress={() =>
+        router.push({
+          pathname: "/checklist/[checklist]",
+          params: { checklist: list.id },
+        })
+      }
+      style={({ pressed }) =>
+        pressed ? [styles.link, styles.linkPressed] : styles.link
+      }
     >
       <Text>{list.title}</Text>
-    </Link>
+    </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  link: {
+    borderColor: "black",
+    borderWidth: 1,
+    width: "100%",
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  linkPressed: {
+    backgroundColor: "lightgray",
+  },
+});
---
+++ components/__tests__/ChecklistLink.test.tsx
@@ -0,0 +1,34 @@
+import { render } from "@testing-library/react-native";
+import ChecklistLink from "../ChecklistLink";
+import { ChecklistType } from "@/types";
+
+const testList: ChecklistType = {
+  id: "123abc",
+  title: "default checklist",
+  entries: [
+    {
+      id: "456def",
+      status: false,
+      title: "item 1",
+      required: true,
+      childOf: undefined,
+      parentTo: undefined,
+    },
+    {
+      id: "789ghi",
+      status: false,
+      title: "item 2",
+      required: true,
+      childOf: undefined,
+      parentTo: undefined,
+    },
+  ],
+};
+
+describe("<ChecklistLink />", () => {
+  test("ChecklistLink renders correctly", () => {
+    const tree = render(<ChecklistLink list={testList} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
