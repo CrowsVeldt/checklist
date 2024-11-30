85cf902b8276e635ac8e957944a1a2857c1780dd
CrowsVeldt <zechy.mahler@gmail.com>
2024-11-30 11:37:10 -0700

    feat: add deleteModal to homescreen, trigger when delete button is pressed

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import AddButton from "@/components/AddButton";
 import ChecklistLink from "@/components/ChecklistLink";
 import { AppContext } from "@/context/AppContext";
 import { ChecklistType } from "@/utils/types";
-import { ContextType, useContext } from "react";
-import { StyleSheet } from "react-native";
+import { ContextType, useContext, useState } from "react";
+import { Modal, Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 /* 
@@ -16,14 +16,55 @@ import { SafeAreaView } from "react-native-safe-area-context";
 */
 
 export default function Index() {
-  const { getLists }: ContextType<typeof AppContext> = useContext(AppContext);
+  const { getLists, deleteList }: ContextType<typeof AppContext> =
+    useContext(AppContext);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [modalTarget, setModalTarget] = useState<ChecklistType | undefined>();
   const lists: ChecklistType[] = getLists();
+
+  const removeList = (list: ChecklistType) => {
+    console.log(modalTarget)
+  };
+
   return (
     <SafeAreaView style={styles.mainView}>
-      {lists &&
-        lists.map((list, index) => {
-          return <ChecklistLink list={list} />;
-        })}
+      <Modal visible={modalVisible} style={styles.modal} animationType="fade">
+        <Text>Are you sure you want to delete the list?</Text>
+        <View style={styles.buttonContainer}>
+          <Pressable
+            style={[styles.modalButton, styles.confirmDeletion]}
+            onPress={() => {
+              removeList(modalTarget!)
+              setModalVisible(false);
+            }}
+          >
+            <Text style={styles.modalButtonText}>Yes</Text>
+          </Pressable>
+          <Pressable
+            style={[styles.modalButton]}
+            onPress={() => {
+              setModalTarget(undefined);
+              setModalVisible(false);
+            }}
+          >
+            <Text style={styles.modalButtonText}>No</Text>
+          </Pressable>
+        </View>
+      </Modal>
+      <View>
+        {lists &&
+          lists.map((list, index) => {
+            return (
+              <ChecklistLink
+                list={list}
+                triggerModal={(list: ChecklistType) => {
+                  setModalTarget(list);
+                  setModalVisible(true);
+                }}
+              />
+            );
+          })}
+      </View>
       <AddButton />
     </SafeAreaView>
   );
@@ -33,5 +74,29 @@ const styles = StyleSheet.create({
   mainView: {
     flex: 1,
     alignItems: "center",
+  },
+  modal: {
+    height: 200,
+    width: 200,
+  },
+  modalButton: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    width: 100,
+    height: 40,
+  },
+  modalButtonText: {
+    textAlign: "center",
+    textAlignVertical: "center",
+  },
+  confirmDeletion: {
+    backgroundColor: "red",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
   },
 });
