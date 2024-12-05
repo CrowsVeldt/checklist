e7bcbbd8f657376ec8a72a861b1444e2dcc8ec94
CrowsVeldt <zechy.mahler@gmail.com>
2024-12-05 10:13:12 -0700

    dev: format _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,13 +4,21 @@ import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <AppProvider>
-      <StatusBar style="dark" />
-      <Stack>
-        <Stack.Screen name="index" options={{title: "Checklists"}} />
-        <Stack.Screen name="create/index" options={{title: "Create Checklist"}} />
-        <Stack.Screen name="edit/index" options={{title: "Edit Checklist"}} />
-        </Stack>;
-    </AppProvider>
+    <>
+      <AppProvider>
+        <StatusBar style="dark" />
+        <Stack>
+          <Stack.Screen name="index" options={{ title: "Checklists" }} />
+          <Stack.Screen
+            name="create/index"
+            options={{ title: "Create Checklist" }}
+          />
+          <Stack.Screen
+            name="edit/index"
+            options={{ title: "Edit Checklist" }}
+          />
+        </Stack>
+      </AppProvider>
+    </>
   );
 }
