a8c8a2bd9499af90badeb38d886716aed93c52a6
CrowsVeldt <zechy.mahler@gmail.com>
2024-12-04 18:07:26 -0700

    feat: add amount finished to checklist title

--- app/checklist/[checklist].tsx
+++ app/checklist/[checklist].tsx
@@ -2,21 +2,26 @@ import ChecklistEntryItem from "@/components/ChecklistEntryItem";
 import { AppContext } from "@/context/AppContext";
 import { ChecklistType } from "@/utils/types";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { ContextType, useContext } from "react";
+import { ContextType, useContext, useState } from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Checklist() {
   const { getListById }: ContextType<typeof AppContext> =
     useContext(AppContext);
+  const [numberComplete, setNumberComplete] = useState<number>(0);
   const { checklist }: { checklist: string } = useLocalSearchParams();
   const list: ChecklistType | undefined = getListById(checklist);
 
   return (
     <SafeAreaView style={styles.page}>
-      <Stack.Screen options={{ title: list!.title }} />
+      <Stack.Screen
+        options={{
+          title: `${list?.title} - ${numberComplete}/${list?.entries.length}`,
+        }}
+      />
       {list &&
-        list.entries.map((entry) => (
+        list.entries.map((entry, index) => (
           <ChecklistEntryItem
             title={entry.title}
             required={entry.required}
