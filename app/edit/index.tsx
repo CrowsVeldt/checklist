a3fdc1cb2cdb184a5490e2d6c90025aebcf82ea5
CrowsVeldt <zechy.mahler@gmail.com>
2024-12-07 16:31:35 -0700

    ui: improve edit list item styles

--- app/edit/index.tsx
+++ app/edit/index.tsx
@@ -55,15 +55,17 @@ export default function EditChecklist() {
         <TitleInput title={title} setTitle={setTitle} />
         <View>
           <FlatList
-          style={styles.list}
+            style={styles.list}
             data={entries}
-            renderItem={({ item }) => (
+            renderItem={({ item, index }) => (
               <ChecklistItemInput
                 id={item.id}
                 initialTitle={item.title}
                 initialRequired={item.required}
                 onEntryChange={onEntryInputChange}
                 remove={() => removeEntry(item.id)}
+                first={index === 0}
+                last={index === entries.length - 1}
               />
             )}
             keyExtractor={(item) => item.id}
@@ -115,7 +117,7 @@ const styles = StyleSheet.create({
     width: "80%",
   },
   list: {
-    height: "70%"
+    height: "70%",
   },
   updateButton: {
     borderWidth: 1,
