0ad6505557dc5812c9a50fa5b08205b59b7b5c15
CrowsVeldt <zechy.mahler@gmail.com>
2024-12-07 06:47:30 -0700

    dev: add remove to checklistItemInput test

--- components/__tests__/ChecklistItemInput.test.tsx
+++ components/__tests__/ChecklistItemInput.test.tsx
@@ -8,6 +8,7 @@ describe("<ChecklistItemInput />", () => {
         id="abc"
         initialTitle="title"
         onEntryChange={() => true}
+        remove={() => undefined}
       />
     ).toJSON();
 
